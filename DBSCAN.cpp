01f596564614737335a571f31e526ab2cdd8f63a
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2025-06-06 01:45:12 +0200

    :tada: initial implementation

---
+++ DBSCAN.cpp
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#include <cstdint>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+struct Point
+{
+	double x;
+	double y;
+	
+	std::string toString() {
+		return ("{" + std::to_string(x) + ", " + std::to_string(y) + "}");
+	}
+};
+
+constexpr int UNDEFINED = 0;
+constexpr int NOISE = -1;
+
+auto distFunc = [](const Point& p, const Point& q)
+{
+	return sqrt(
+		pow(p.x - q.x, 2) + pow(p.y - q.y, 2)
+	);
+};
+
+template<typename DistanceFunction>
+std::vector<int> RangeQuery(const std::vector<Point>& DB, const DistanceFunction distFunc, const Point& q, double eps)
+{
+	std::vector<int> neighbors;
+	
+	for (uint32_t i = 0; i < DB.size(); ++i)
+	{
+		if (distFunc(DB[i], q) <= eps)
+		{
+			neighbors.push_back(i);
+		}
+	}
+	
+	return neighbors;
+}
+
+// New Function to union two vectors, regular set_union() didn't work
+std::vector<int> vector_union(std::vector<int> a, const std::vector<int>& b) {
+	std::sort(a.begin(), a.end());
+	std::vector<int> sorted_b = b;
+	std::sort(sorted_b.begin(), sorted_b.end());
+	std::vector<int> result;
+	std::set_union(a.begin(), a.end(), sorted_b.begin(), sorted_b.end(), std::back_inserter(result));
+	return result;
+}
+
+template<typename DistanceFunction>
+std::vector<int> DBSCAN(
+	const std::vector<Point>& DB,
+	DistanceFunction distFunc,
+	const double eps,
+	const uint32_t minPts
+) {
+	std::vector<int> labels(DB.size(), UNDEFINED);
+	uint32_t C = 0;
+
+	for (uint32_t p = 0U; p < DB.size(); ++p)
+	{
+		if (labels[p] != UNDEFINED)
+		{
+			continue;
+		}
+
+		std::vector<int> neighbors = RangeQuery(DB, distFunc, DB[p], eps);
+
+		if (neighbors.size() < minPts)
+		{
+			labels[p] = NOISE;
+			continue;
+		}
+
+		C += 1;
+		labels[p] = C;
+
+		while (!neighbors.empty())
+		{
+			auto q = neighbors.back();
+			neighbors.pop_back();
+
+			if (labels[q] == NOISE)
+			{
+				labels[q] = C;
+			}
+
+			if (labels[q] != UNDEFINED)
+			{
+				continue;
+			}
+
+			labels[q] = C;
+
+			auto neighbors_of_neighbors = RangeQuery(DB, distFunc, DB[q], eps);
+
+			if (neighbors_of_neighbors.size() >= minPts)
+			{
+				neighbors = vector_union(neighbors, neighbors_of_neighbors);
+			}
+		}
+	}
+
+	return labels;
+}
+
+int main(int argc, char const *argv[]) {
+	
+	std::vector<Point> DB = {
+		{-2.155, -3.195}, {-2.635, -4.605}, {-4.285, -5.025}, {-4.795, -6.825},
+		{-3.925, 1.665},
+		{1.355, 4.005}, {2.975, 4.665}, {2.615, 3.915}, {1.775, 3.345}, {1.895, 4.575}, {2.255, 2.415}, {2.735, 3.105},
+		{5.795, -1.275}, {3.275, -4.485}
+	};
+
+	const auto eps = 4.0;
+	const auto minPts = 3U;
+	
+	std::cout << "-=: All Points :=-\n";
+	for (Point p : DB)
+	{
+		std::cout << p.toString() << '\n';
+	}
+	std::cout << "-=: End :=-\n\n";
+
+
+
+	auto labels = DBSCAN(DB, distFunc, eps, minPts);
+	
+	
+	
+	std::cout << "-=: After DBSCAN, Cluster IDs :=-\n";
+	for (int p : labels)
+	{
+		std::cout << p << '\n';
+	}
+	std::cout << "-=: End :=-\n";
+
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# DBSCAN in C++
+
+This is an adaptation of [Modern C++](https://www.youtube.com/@moderncpp)'s DBSCAN implementation.
+
+[Video link here](https://www.youtube.com/watch?v=87ogbzhXUxo)
+
+The set_union() function was the only thing I really had to change, adding vector_union(), otherwise it is the same code that now prints out the data Points and their Cluster IDs.
